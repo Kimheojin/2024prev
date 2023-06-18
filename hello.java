77798f314e435e2b60befe69163e02b5d95efd46
jin <hurjin1109@naver.com>
2023-06-18 16:26:16 +0900

    hello

---
+++ hello.java
@@ -0,0 +1,35 @@
+class Hello{//파일 이름이랑 같아야 한다(대소문자 까지)
+    public static void main(String[] args){/*
+        System.out.println("hello, world");
+        
+        int year = 0;
+        int age = 14;
+
+        System.out.println(year);
+        System.out.println(age);
+
+        year = age + 2000;
+        age = age + 1;
+
+        System.out.println(year);
+        System.out.println(age);
+        */
+        //변수의 명명 규칙
+        //대소문자가 구분되며 길이에 제한이 없다
+        //숫자로 시작해서는 안된다.
+        //특수문자는 _와 $만 허용한다
+
+        //클래스 이름의 첫 글자는 항상 대문자로 한다
+        //여러 단어로 이루어진 이름은 단어의 첫 글자를 대문자로 한다.
+        //상수의 이름은 모두 대문자로 한다. 여러 단어로 이루어진 경우 _를 사용한다
+        //기본형과 참조형
+        //기본형
+        //논리형 
+        //문자형 char 
+        //상수 사용 final
+        final int Max_speed = 10;
+
+        System.out.println(Max_speed);
+
+    }
+}//java로 프로그램 작성하기
