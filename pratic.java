48dd2972a4727376d0b9ba29b37b5f3d3d4c1a67
jin <hurjin1109@naver.com>
2023-06-18 18:41:54 +0900

    4horts

---
+++ pratic.java
@@ -0,0 +1,22 @@
+public interface pratic {
+    public static void main(String[] args){
+        //이부분에 프로그램 
+        /* 
+        System.out.println(2*5);//3출력
+ 
+        //문자와 문자열은 다르다 '' -> 문자
+        int a;
+        a = 1;
+        System.out.println(a + 1);
+        a = 2;
+        System.out.println();
+        
+
+        String first;//문자열만 가능
+        first = "coding";//한번에도 가능
+        Sring second = "coding";
+         */
+
+
+    }    
+} 
