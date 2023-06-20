93fcdc309a1886a56611a27acee5f8a171f1ae00
jin <hurjin1109@naver.com>
2023-06-20 15:05:21 +0900

    ㅁㅇㄴㄹ

--- method1.java
+++ method1.java
@@ -9,8 +9,8 @@ public class method1 {
     }
     public static void main(String[] args){
         numbering(1, 5);//순서도 지켜야 한다
-        
+
 
     }
-    
+    //main 함수 부분에서 어떻게 실행하는 지 먼저 본 다음에 하기
 }
