150fc22e2f0ca0ef68da5065b0b23a70347a6022
jin <hurjin1109@naver.com>
2023-06-22 14:03:33 +0900

    qwer

--- algol1427.java
+++ algol1427.java
@@ -1,12 +1,14 @@
 import java.util.Scanner;
 class mergesort{
     int digit, solve;
+    int list[];
 
     public void setoperand(){
         Scanner arrangement1 = new Scanner(System.in);
         int array = arrangement1.nextInt();
         this.digit = array;
         arrangement1.close();
+     
     }
 
     
@@ -14,7 +16,7 @@ class mergesort{
         
     }
     public void print_solve(){
-        
+
     }
     //그냥 안에 그거 하는 함수만 넟쟈
 }
@@ -24,8 +26,7 @@ public class algol1427 {
     public static void main(String[] args){
 
         mergesort a1 = new mergesort();
-
-        a1.setoperand();//정렬할 배열을 입력받음
+        System.out.println(a1.digit);//할 배열을 입력받음
         a1.solve();
         a1.print_solve();
     }
