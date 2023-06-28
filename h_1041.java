21e7e6d326cb97c3f6740d1aa1091a795f01a012
jin <hurjin1109@naver.com>
2023-06-28 17:15:03 +0900

    asdf

---
+++ h_1041.java
@@ -0,0 +1,24 @@
+class Tv{
+    boolean power;//전원상태(on/off)
+    int channel;
+    
+    void power();
+    void channelup();
+    void channeldown();
+}
+//클래스 간의 관계 - 포함관계
+class CIrcle{
+    int x;
+    int y;
+    int r;
+}
+class Point{
+    int x;
+    int y;
+}
+
+public class h_1041{
+    public static void main(String[] args){
+        
+    }
+}
