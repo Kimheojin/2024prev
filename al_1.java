c8c7e013a6f69bc1287f1cada88b0bde7e7bd0aa
jin <hurjin1109@naver.com>
2023-07-05 16:45:50 +0900

    asdf

---
+++ al_1.java
@@ -0,0 +1,14 @@
+class Calculator{
+    int left, right;
+
+    public void setOprands(int left, int right){
+        this.left = left;
+        this.right = right;
+    }
+    public void divide(){
+        try{
+            System.out.print("계산결과는");
+            
+        }
+    }
+}
