106eda9ff59f41019635a9607d3e38143e220ea0
jin <hurjin1109@naver.com>
2023-07-16 23:55:31 +0900

    asdfasdf

---
+++ al_18108.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class al_18108 {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+
+        int year = sc.nextInt();
+
+        sc.close();
+
+        year = year - 543;
+
+        System.out.println(year);
+    }
+}
