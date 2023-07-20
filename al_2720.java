ac731c420e0d1ff9e8e316cd5573d56b779b8d9d
jin <hurjin1109@naver.com>
2023-07-20 16:02:14 +0900

    asdfasdf

---
+++ al_2720.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class al_2720 {
+    public static void main(String args[]) throws IOException{
+        
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int source;
+
+        for(int i = 0; i < N; i++){
+            source = Integer.parseINt(br.readLine());
+            
+            bw.write();
+            
+        }
+
+        bw.flush();
+        bw.close();
+    }
+}
