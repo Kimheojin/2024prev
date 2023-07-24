0e68d80ca3c9602cf5f39cadb91aead522feaa01
jin <hurjin1109@naver.com>
2023-07-24 18:53:20 +0900

    ㄴㅇ

---
+++ al_1193.java
@@ -0,0 +1,17 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class al_1193 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //홀수면 앞에서 시작
+        //짝수면 뒤에서 시작
+
+        while(true){
+            
+        }
+
+    }
+        
+}
--- al_24265.java
+++ al_24265.java
@@ -1,19 +1,11 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
-public class al_2231 {
-    public static void main(String args[]) throws Exception{
+public class al_24265 {
+    public static void main(String args[]) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        S
         
 
-		}
     }
- 
-	
-        
-    
-    
-
+}
