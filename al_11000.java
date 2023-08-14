dbe00ba1e5de1c82d92e298221a34d0ada7171cc
jin <hurjin1109@naver.com>
2023-08-14 10:10:04 +0900

    asdf

---
+++ al_11000.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class al_11000 {
+    public static void main(String args[])throws IOException{
+    
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        //수강 신청의 마스터 김종혜 선생님
+        //새로운 S T 주어지는데 최소의 강의실을 사용해서 모든 수업을
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine(), " ");
+        for(int )
+    
+    }
+}
--- hello.java
+++ hello.java
@@ -1,63 +1,7 @@
-class Hello{//파일 이름이랑 같아야 한다(대소문자 까지)
-    public static void main(String[] args){/*
-        System.out.println("hello, world");
-        
-        int year = 0;
-        int age = 14;
+import java.io.IOException;
 
-        System.out.println(year);
-        System.out.println(age);
-
-        year = age + 2000;
-        age = age + 1;
-
-        System.out.println(year);
-        System.out.println(age);
-        */
-        //변수의 명명 규칙
-        //대소문자가 구분되며 길이에 제한이 없다
-        //숫자로 시작해서는 안된다.
-        //특수문자는 _와 $만 허용한다
-
-        //클래스 이름의 첫 글자는 항상 대문자로 한다
-        //여러 단어로 이루어진 이름은 단어의 첫 글자를 대문자로 한다.
-        //상수의 이름은 모두 대문자로 한다. 여러 단어로 이루어진 경우 _를 사용한다
-        //기본형과 참조형
-        //기본형
-        //논리형 
-        //문자형 char 
-        //상수 사용 final
-        /* 
-        final int Max_speed = 10;
-
-        System.out.println(Max_speed);
-        System.out.println(Max_speed);
-        
-
-        int i = 3;
-        i++;
-        System.out.println(i);
-        i++;
-        System.out.println(++i);
-        System.out.println(++i);
-        System.out.println(++i);
-        System.out.println(i);
-        
-        if(true){
-            System.out.println(1);
-        }
-        else{
-            System.out.println(2);
-        }
-        
-        String id = args[0];
-        if(id.equals("egoing")){
-            System.out.println("right");
-        }else{
-            System.out.println("wrong");
-        }
-        */
-    
-        
+public class hello {
+    public static void main(String args[]) throws IOException{
+        System.out.println("hello");
     }
-}//java로 프로그램 작성하기
+}
