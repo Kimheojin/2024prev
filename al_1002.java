849829b05317bc7e715ae24a7162afc4f2be1856
jin <hurjin1109@naver.com>
2023-08-15 14:25:28 +0900

    asdf

---
+++ al_1002.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class al_1002 {
+    public static void main(String args[]){
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+    }
+    
+}
