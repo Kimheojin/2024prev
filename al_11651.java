9da78e65c15734a81cbfc657c9d3fc2156ff759b
jin <hurjin1109@naver.com>
2023-09-17 14:14:58 +0900

    asdfasdf

--- al_11651.java
+++ al_11651.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class al_11651 {
     public static void main(String args[]) throws IOException{
@@ -8,9 +9,18 @@ public class al_11651 {
 
         int n = Integer.parseInt(br.readLine());
 
+        int[][] Array_1 = new int[n][2];
+
+        StringTokenizer st;
+
         for(int i = 0; i < n; i++){
-            
+            st = new StringTokenizer(br.readLine(), " ");
+
+            Array_1[i][0] = Integer.parseInt(st.nextToken()); //x
+            Array_1[i][1] = Integer.parseInt(st.nextToken()); //y
         }
+
+    
         
     }
     
