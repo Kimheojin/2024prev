359d74a324f6f7bbeabf24d101410f1a61a30d81
jin <hurjin1109@naver.com>
2023-10-28 16:24:00 +0900

    asdf

--- al_pr.java
+++ al_pr.java
@@ -1,5 +1,6 @@
 public class al_pr {
-    public static void main(String args[]){
-        
+    public static void main(String args[]){//그리디 알고리즘 문제
+        //각각에서 최적의 해를 찾는 문제이다
+        Buffered Reader br = new BufferedReader(new INputStreamReader(S))
     }
 }
